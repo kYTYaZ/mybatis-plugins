64e49a320e1084af8845039229af6a62f8f225ac
bailu.zjj <bailu.zjj@alibaba-inc.com>
2018-11-08 10:51:52 +0800

    x

--- src/main/java/com/fishqq/mybatis/dynamic/MapperFactory.java
+++ src/main/java/com/fishqq/mybatis/dynamic/MapperFactory.java
@@ -4,8 +4,6 @@ import java.util.function.Function;
 
 import javax.sql.DataSource;
 
-import com.alibaba.dt.onedata.datasource.DataSourceTypeEnum;
-
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
@@ -18,20 +16,19 @@ import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
  * @author 白路 bailu.zjj@alibaba-inc.com
  * @date 2018/9/5
  */
-public interface MapperFactory<Mapper> {
-    Configuration getConfig(DataSourceTypeEnum type, DataSourceTypeEnum metaType);
+public interface MapperFactory<Type, Mapper> {
+    Configuration getConfig(Type type);
 
     Class<Mapper> getMapperType();
 
-    default <T> T callMapper(DataSourceTypeEnum type,
-                             DataSourceTypeEnum metaType,
+    default <T> T callMapper(Type type,
                              DataSource dataSource,
                              Function<Mapper, T> api) {
         TransactionFactory trxFactory = new JdbcTransactionFactory();
 
         Environment env = new Environment("dev", trxFactory, dataSource);
 
-        Configuration config = new ConfigurationWrapper(this.getConfig(type, metaType), env);
+        Configuration config = new ConfigurationWrapper(this.getConfig(type), env);
 
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(config);
 
